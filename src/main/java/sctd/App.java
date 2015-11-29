fc30f5739766f043ba865214b005c6149fe56fa8
Steven <vstrength@gmail.com>
2015-11-29 15:00:07 -0800

    Initial skeleton

---
+++ src/main/java/sctd/App.java
@@ -0,0 +1,12 @@
+package sctd;
+
+/**
+ * Hello world!
+ */
+public final class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
+
---
+++ src/test/java/sctd/AppTest.java
@@ -0,0 +1,17 @@
+package sctd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public final class AppTest {
+
+  @Test
+  public void shouldPass()
+  {
+    assertTrue(true);
+  }
+}
